25f3870253b77ef69fa4c253a4c72910cf6c5461
unknown <btkkentxx2002@gmail.com>
2022-06-28 14:43:19 +0700

    chuyen C sang F

---
+++ Code.js
@@ -0,0 +1,9 @@
+function cSangF() {
+    let vao = +document.getElementById('doC').value
+    let ra = (vao * 9 / 5 + 32);
+    document.getElementById('doF').value=ra
+
+}
+function reset(){
+    window.location.reload();
+}
